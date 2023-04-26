680e1cffa800d7b5eb0fa4f1a21f84c7e95eef5e
BenSchZA <BenSchZA@users.noreply.github.com>
2023-04-26 14:46:48 +0100

    Add scopes to creds

--- analytics_bot_langchain/app.py
+++ analytics_bot_langchain/app.py
@@ -5,7 +5,10 @@ from google.oauth2 import service_account
 import streamlit as st
 
 
-credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"]).with_scopes([
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/bigquery",
+])
 client = bigquery.Client(credentials=credentials)
 agent = create_bigquery_agent(ChatOpenAI(temperature=0), bigquery_client=client, verbose=True)
 
