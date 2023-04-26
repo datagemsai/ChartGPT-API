2483f6975ba40cc7021bca53149124272d13ffef
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-04-26 13:27:19 +0200

    features: move prompt update to analytics_bot_langchain/agents/agent_toolkits/bigquery/prompts/v2.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,17 +29,6 @@ class StreamlitWriter:
 
 
 sample_questions = {
-    "nft_lending_aggregated_users": [
-        "Plot daily users for nftfi, x2y2 and arcade",
-        "Plot the weekly distribution of unique users over time",
-        "On what date x2y2 had the highest number of users?",  # data request
-    ],
-    "nft_lending_aggregated_borrow": [
-        "Plot the borrow volume over time for nftfi, benddao, arcade, x2y2, jpegd",
-    ],
-    "nftfi_loan_data": [
-        "Plot the loan principal amount of the top 5 asset classes by volume over time",
-    ],
     "dune_dataset": [
         "Plot the borrow volume across the protocols nftfi, benddao, arcade, jpegd from December 2022 to March 2023",
         "Plot the 30-day median number of users starting from August 2022",
@@ -53,6 +42,18 @@ sample_questions = {
         # "Give one powerful visualization on dune_dataset which a primary and secondary y-axis or stacked bar charts, and return it with good layout including legend and colors. You may use log y axis",
         # "Give two powerful visualization on dune_dataset which use primary and secondary y-axis and return it with good layout including legend and colors",
     ],
+    "nft_lending_aggregated_users": [
+        "Plot daily users for nftfi, x2y2 and arcade",
+        "Plot the weekly distribution of unique users over time",
+        "On what date x2y2 had the highest number of users?",  # data request
+    ],
+    "nft_lending_aggregated_borrow": [
+        "Plot the borrow volume over time for nftfi, benddao, arcade, x2y2, jpegd",
+    ],
+    "nftfi_loan_data": [
+        "Plot the loan principal amount of the top 5 asset classes by volume over time",
+    ],
+    "run_all_dune": None,
 }
 
 
