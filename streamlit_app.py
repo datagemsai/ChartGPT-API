766d87e5bc0fe5efa6c5670114cd3344d63623f4
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-04-26 14:12:05 +0200

    features:  - chartbot: move __init__.py logic to new app.py  - streamlit_app.py: add a partial function to allow the user to choose between langchain and chartbot (defaulting to langchain)  - remove streamlit_app_chartbot.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 
 import streamlit as st
+from functools import partial
 
 # Custom imports
-from analytics_bot_langchain.agents import run
+import analytics_bot_langchain
+import analytics_bot
 """
 # NFTfi Analytics AGI
 """
@@ -31,15 +33,15 @@ class StreamlitWriter:
 sample_questions = {
     "dune_dataset": [
         "Plot the borrow volume across the protocols nftfi, benddao, arcade, jpegd from December 2022 to March 2023",
+        "Give three visualizations from dune_dataset",
         "Plot the 30-day median number of users starting from August 2022",
         "Plot the weekly NFT finance market share percentage by unique users",
         "On what date x2y2 had the highest number of users?",
         "Plot the weekly distribution of unique users over time",
         "Using dataset 'dune_dataset', plot the borrow volume over time for nftfi, benddao, arcade, jpegd",
         "Plot the total loan volume per day using Plotly",
-        "Give three visualizations from dune_dataset",
         "Return a chart which will help me understand user dynamics across protocols",
-        # "Give one powerful visualization on dune_dataset which a primary and secondary y-axis or stacked bar charts, and return it with good layout including legend and colors. You may use log y axis",
+        "Give one powerful visualization on dune_dataset which a primary and secondary y-axis or stacked bar charts, and return it with good layout including legend and colors. You may use log y axis",
         # "Give two powerful visualization on dune_dataset which use primary and secondary y-axis and return it with good layout including legend and colors",
     ],
     "nft_lending_aggregated_users": [
@@ -56,13 +58,26 @@ sample_questions = {
     "run_all_dune": None,
 }
 
+sample_agents = {
+    'Langchain': analytics_bot_langchain.agents.run,
+    'Chartbot': analytics_bot.run,
+}
+
 
 with st.echo(code_location='above'):
+    agent = st.selectbox('Select agent:', sample_agents.keys())
+
     dataset_id = st.selectbox('Select dataset:', sample_questions.keys())
-    
+
     sample_questions_for_dataset = [""]  # Create unselected option
     sample_questions_for_dataset.extend(sample_questions[dataset_id])
     sample_question = st.selectbox('Select sample question (optional):', sample_questions_for_dataset)
+
+    run = sample_agents[agent]
+    if agent == 'Chartbot':
+        # Charbot run function (i.e. analytics_bot.run) has more arguments than analytics_bot_langchain.agents.run,
+        # so create a partial function of it to match signatures.
+        run = partial(run, dataset_id=dataset_id, project_id=BQ_PROJECT_ID)
 
     if not sample_question:
         question = st.text_input("Enter your question:")
