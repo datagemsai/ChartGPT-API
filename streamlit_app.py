00488c74eafe27ff09af5df7375e379473b18290
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-04-27 14:40:19 +0200

    features: remove "using / from 'dune_dataset'" in sample questions

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,15 +14,15 @@ ENV = st.secrets["ENV"]
 sample_questions = {
     "dune_dataset": [
         "Plot the borrow volume across the protocols nftfi, benddao, arcade, jpegd from December 2022 to March 2023",
-        "Give three visualizations from dune_dataset",
+        "Give three visualizations",
         "Plot the 30-day median number of users starting from August 2022",
         "Plot the weekly NFT finance market share percentage by unique users",
-        "On what date x2y2 had the highest number of users?",
         "Plot the weekly distribution of unique users over time",
-        "Using dataset 'dune_dataset', plot the borrow volume over time for nftfi, benddao, arcade, jpegd",
+        "Plot the borrow volume over time for nftfi, benddao, arcade, jpegd",
         "Plot the total loan volume per day using Plotly",
         "Return a chart which will help me understand user dynamics across protocols",
-        "Give one powerful visualization on dune_dataset which a primary and secondary y-axis or stacked bar charts, and return it with good layout including legend and colors. You may use log y axis",
+        "Give one powerful visualization using a primary and secondary y-axis or stacked bar charts, and return it with good layout including legend and colors. You may use log y axis",
+        "On what date x2y2 had the highest number of users?",
         # "Give two powerful visualization on dune_dataset which use primary and secondary y-axis and return it with good layout including legend and colors",
     ],
     "nft_lending_aggregated_users": [
