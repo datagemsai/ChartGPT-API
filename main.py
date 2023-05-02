b34dc6263152db9f43a9a1fe26a900e259ae382f
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-05-02 11:14:15 +0200

    features: add comment

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ tables_id = {
 
 def query_dune_api_and_save_dataset_to_bq(table_name: str, query_id: int, datatype: Datatype, dune_query: bool):
     if dune_query:
+        # if we query dune API, we save .csv locally and only upload that table to BQ. else we upload all tables in data/dune/nftfi/*.csv to BQ
         run_query(file_name=table_name, datatype=datatype, query_id=query_id)
     clean_csv_files_and_save_to_bigquery(table_name=table_name, datatype=datatype, dune_query=dune_query)
 
