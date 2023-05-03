9bed432c974747c636b53258229b41fb64315861
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-05-03 11:18:44 +0200

    features: update datasets dict with new naming, default questions

--- app/config/default.py
+++ app/config/default.py
@@ -1,8 +1,28 @@
 datasets = {
-    "dex": [
-        "Create a plot to provide an understanding of swaps across blockchains",
-        "Plot the five most traded token pairs",
-        "Give three visualizations",
+    "ethereum_dex_transactions": [
+        "Plot a pie chart of the top 5 takers with highest transaction count, group the remainder takers as Others category",
+        "Plot top 5 projects by transaction count",
+        "Plot the pairs corresponding to the largest USDC transactions",
+        "Plot the highest USD transactions grouped by blockchain",
+        "Plot the highest trade count grouped by blockchain and trading pair"
     ],
-    "nft_lending_aggregated_borrow": []
+    "nft_lending_aggregated_borrow": [
+        "Plot the monthly loan volume grouped by protocol",
+        "Plot a stacked bar chart of loan volume grouped by protocol since August 2022",
+        "Plot top 3 protocols on April 3rd 2023",
+        "Plot daily borrow volumes for each protocol in February 2023",
+        "Plot monthly cumulative borrow volumes for each protocol",
+    ],
+    "growjo_fatest_growing_companies": [
+        "Plot total funding by country",
+        "Plot the average valuation for US Analytics companies by State",
+        "Plot top 5 companies by job openings",
+        "What is the average number of employees for AI companies founded between 2020 and 2022 ",
+        "Plot the average revenue for CA, TX, MA, NY state companies",
+    ]
+
 }
+
+# "Create a plot to provide an understanding of swaps across blockchains",
+# "Plot the five most traded token pairs",
+# "Give three visualizations",
