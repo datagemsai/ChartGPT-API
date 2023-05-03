893111eb2f296b18cdd8323a1107553acbd82f39
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-05-03 14:53:46 +0200

    features: default question "What is the average number of employees for AI companies founded between 2020 and 2022 " -> "What is the average number of employees for AI companies from CA state",

--- app/config/default.py
+++ app/config/default.py
@@ -17,7 +17,7 @@ datasets = {
         "Plot total funding by country",
         "Plot the average valuation for US Analytics companies by State",
         "Plot top 5 companies by job openings",
-        "What is the average number of employees for AI companies founded between 2020 and 2022 ",
+        "What is the average number of employees for AI companies from CA state",
         "Plot the average revenue for CA, TX, MA, NY state companies",
     ]
 
