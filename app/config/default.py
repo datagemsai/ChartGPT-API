0e8938136b1425ef33f2bdae573a63a89b0aa79c
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-05-03 15:05:43 +0200

    refactor: python PEP8

--- app/config/default.py
+++ app/config/default.py
@@ -12,28 +12,29 @@ class Dataset:
     def __repr__(self):
         return self.name
 
+
 datasets = [
     Dataset(
-        name = "Ethereum Decentralized Exchange Transactions",
+        name="Ethereum Decentralized Exchange Transactions",
         # TODO Rename as follows
         # id = "ethereum_dex_transactions",
-        id = "ethereum_dex_transactions",
-        description = "A dataset of decentralized exchange (DEX) transactions on the Ethereum Blockchain.",
-        sample_questions = [
+        id="ethereum_dex_transactions",
+        description="A dataset of decentralized exchange (DEX) transactions on the Ethereum Blockchain.",
+        sample_questions=[
             "Plot a pie chart of the top 5 takers with highest transaction count, group the remainder takers as Others category",
             "Plot top 5 projects by transaction count",
             "Plot the pairs corresponding to the largest USDC transactions",
             "Plot the highest USD transactions grouped by blockchain",
-            "Plot the highest trade count grouped by blockchain and trading pair"
+            "Plot the highest trade count grouped by blockchain and trading pair",
         ]
     ),
     Dataset(
-        name = "NFT Lending Protocol Aggregate Borrow Volume",
+        name="NFT Lending Protocol Aggregate Borrow Volume",
         # TODO Rename as follows
         # id = "nft_lending_aggregate_borrow_volume",
-        id = "nft_lending_aggregated_borrow",
-        description = "A dataset of the aggregate borrow volume for different NFT lending protocols.",
-        sample_questions = [
+        id="nft_lending_aggregated_borrow",
+        description="A dataset of the aggregate borrow volume for different NFT lending protocols.",
+        sample_questions=[
             "Plot the monthly loan volume grouped by protocol",
             "Plot a stacked bar chart of loan volume grouped by protocol since August 2022",
             "Plot top 3 protocols on April 3rd 2023",
@@ -42,10 +43,10 @@ datasets = [
         ],
     ),
     Dataset(
-        name = "Growjo - Fastest Growing Companies",
-        id = "growjo_fastest_growing_companies",
-        description = "Analyse the fastest growing companies in the AI & Analytics space according to Growjo",
-        sample_questions = [
+        name="Growjo - Fastest Growing Companies",
+        id="growjo_fastest_growing_companies",
+        description="Analyse the fastest growing companies in the AI & Analytics space according to Growjo",
+        sample_questions=[
             "Plot total funding by country",
             "Plot the average valuation for US Analytics companies by State",
             "Plot top 5 companies by job openings",
