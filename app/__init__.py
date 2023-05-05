039538169d27ccd8c72157f378f564ec4aa1ee3e
BenSchZA <BenSchZA@users.noreply.github.com>
2023-05-05 17:10:10 +0100

    Monkey patch repr methods

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ import os
 from plotly.graph_objs._figure import Figure
 import plotly.io as pio
 import pandas as pd
+from pandas.io.formats import (
+    format as fmt,
+)
+from io import StringIO
 
 
 # Load environment variables from .env
@@ -24,10 +28,33 @@ def pd_display(self):
     if not df_id in st.session_state:
         st.dataframe(self)
         st.session_state[df_id] = 1
-    return ""
+
+    if self._info_repr():
+        buf = StringIO()
+        self.info(buf=buf)
+        return buf.getvalue()
+
+    repr_params = fmt.get_dataframe_repr_params()
+    return self.to_string(**repr_params)
+
 pd.DataFrame.display = lambda self: pd_display(self)
 pd.DataFrame.__repr__ = lambda self: pd_display(self)
 pd.DataFrame._repr_html_ = lambda self: pd_display(self)
+
+pd.core.indexing._iLocIndexer.__repr__ = lambda self: pd_display(self)
+pd.core.indexing._iLocIndexer._repr_html_ = lambda self: pd_display(self)
+
+pd.core.indexing._LocIndexer.__repr__ = lambda self: pd_display(self)
+pd.core.indexing._LocIndexer._repr_html_ = lambda self: pd_display(self)
+
+pd.core.frame.DataFrame.__repr__ = lambda self: pd_display(self)
+pd.core.frame.DataFrame._repr_html_ = lambda self: pd_display(self)
+
+pd.core.series.Series.__repr__ = lambda self: pd_display(self)
+pd.core.series.Series._repr_html_ = lambda self: pd_display(self)
+
+pd.core.base.PandasObject.__repr__ = lambda self: pd_display(self)
+pd.core.base.PandasObject._repr_html_ = lambda self: pd_display(self)
 
 # Monkey patching of Plotly show()
 def st_show(self):
