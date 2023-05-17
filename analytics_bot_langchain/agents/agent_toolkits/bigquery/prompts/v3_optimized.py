242083c3c63047de91cf419e9fbb512ca09abf80
BenSchZA <BenSchZA@users.noreply.github.com>
2023-05-17 09:10:45 +0100

    Add MVP / POC google sheets prompt

--- analytics_bot_langchain/agents/agent_toolkits/bigquery/prompts/v3_optimized.py
+++ analytics_bot_langchain/agents/agent_toolkits/bigquery/prompts/v3_optimized.py
@@ -11,12 +11,21 @@ Utilize ONLY these tools for analysis, following their expected formatting instr
 
 SUFFIX = """
 # Datasets
-Access these datasets, tables, and columns:
+Access these datasets, tables, and columns from BigQuery using the `bigquery_client` object:
 ```
 tables_summary = {tables_summary}
 ```
 
 Validate column names using: tables_summary[dataset_id][table_id].
+
+Or access public Google Sheets using the `google_sheets_client` object:
+```
+spreadsheet = google_sheets_client.open_by_url('https://docs.google.com/spreadsheets/d/167JjRX8hxgMfxc9LYz9Yh6AOH56PlcaU38SjBkzFxmg/')
+spreadsheet_values = spreadsheet.sheet1.get_all_values(value_render_option="UNFORMATTED_VALUE")
+spreadsheet_dataframe = pd.DataFrame.from_records(spreadsheet_values[1:], columns=spreadsheet_values[0])
+
+spreadsheet_dataframe.head()
+```
 
 # Example SQL Query
 
@@ -37,6 +46,7 @@ Do not import or use other libraries.
 
 # Instructions
 - A BigQuery Client in Python, `bigquery_client`, has been initialized and authenticated.
+- A Google Sheets Client in Python, `google_sheets_client`, has been initialized and authenticated.
 - Use the Plotly library for creating charts and plots.
 - Do NOT make DML statements (INSERT, UPDATE, DELETE, DROP, etc.).
 - Check column names using: print(tables_summary[dataset_id][table_id])
