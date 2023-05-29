5a6b4f936b017155c0193130bd4832837bf84f44
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-05-29 12:13:21 +0200

    refactor: analytics_bot_langchain -> langchain

--- analytics_bot_langchain/__init__.py
+++
@@ -1,5 +0,0 @@
-from analytics_bot_langchain.app import get_agent
-
-__all__ = [
-    "get_agent",
-]
--- analytics_bot_langchain/agents/agent_toolkits/__init__.py
+++
@@ -1,5 +0,0 @@
-from analytics_bot_langchain.agents.agent_toolkits.bigquery.base import create_bigquery_agent
-
-__all__ = [
-    "create_bigquery_agent",
-]
