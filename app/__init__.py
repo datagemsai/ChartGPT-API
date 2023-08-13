7a203c740dff85cf2e919dc0eeb3562ac2098f8f
BenSchZA <BenSchZA@users.noreply.github.com>
2023-08-13 11:13:47 +0200

    Add Sentry

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,12 @@ import logging
 import json
 from importlib import import_module
 from firebase_admin import firestore
+import sentry_sdk
+import sys
+from sentry_sdk import capture_exception
+from sentry_sdk import set_user
+from sentry_sdk import set_tag
 
-
-# Display app name
-PAGE_NAME = "ChartGPT"
-st.set_page_config(page_title=PAGE_NAME, page_icon="📈")
-
-# Configure logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 # Load environment variables from .env
 load_dotenv()
@@ -23,6 +20,36 @@ os.environ.update(st.secrets)
 
 ENV = os.environ.get("ENV", "LOCAL")
 URL = os.environ.get("URL", "http://localhost:8501")
+
+# Configure logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+error_util = sys.modules["streamlit.error_util"]
+script_runner = sys.modules["streamlit.runtime.scriptrunner.script_runner"]
+handle_uncaught_app_exception = script_runner.handle_uncaught_app_exception
+def exception_handler(e):
+    set_tag("environment", ENV)
+    set_user({
+        "id": st.session_state.get("user_id", "anonymous"),
+        "email": st.session_state.get("user_email", "anonymous")
+    })
+    capture_exception(e)
+    handle_uncaught_app_exception(e)
+script_runner.handle_uncaught_app_exception = exception_handler
+
+sentry_sdk.init(
+  dsn="https://76086ca93fddeba3df0b1a5512d41ae7@o4505696591544320.ingest.sentry.io/4505696597114880",
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  traces_sample_rate=1.0
+)
+
+# Display app name
+PAGE_NAME = "ChartGPT"
+st.set_page_config(page_title=PAGE_NAME, page_icon="📈")
 
 if ENV == "LOCAL":
     import app_secrets.gcp_service_accounts
