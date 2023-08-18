98034d6a43e364e424e4a509095411e4f26427c1
BenSchZA <BenSchZA@users.noreply.github.com>
2023-08-18 16:36:19 +0200

    Update ChartGPT description

--- app/config/content.py
+++ app/config/content.py
@@ -7,8 +7,12 @@ Effortlessly code in our radCAD framework with the help of radCAD Assistant. Thi
 ai_token_engineer_description = """
 Elevate your token engineering expertise with AI Token Engineer. This advanced assistant will guide you through complex tasks and augment your work to enhance your productivity.
 """
-chartgpt_description = """
+chartgpt_description_v1 = """
 Master the art of data-driven insights with ChartGPT, your dependable resource for generating charts and answering data-centric questions.
 
 Let the bot be your personal data assistant, guiding you through your data exploration.
+"""
+chartgpt_description = """
+Dive deep into our data marketplace, powered by advanced AI-driven conversations.
+Choose a dataset, chat, and visualize data insights like never before.
 """
