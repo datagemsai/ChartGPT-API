3c1f6c85bf5fbf9b78a80f7bed614d3d0c1d4dc4
BenSchZA <BenSchZA@users.noreply.github.com>
2023-09-04 15:26:29 +0200

    Remove old tests

--- tests/test_analytics_bot.py
+++
@@ -1,9 +0,0 @@
-from analytics_bot import run
-
-
-def test_run():
-    dataset_id = "dune_dataset"
-    question = "Plot the loan principal amount grouped by protocol (i.e. nftfi, benddao, arcade)"
-    questions = [{'question': question}]
-
-    run(questions=questions, dataset_id=dataset_id)
