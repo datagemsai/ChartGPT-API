6b1c4d6df3739eadb5337ad6aab5bc142d0021c4
BenSchZA <BenSchZA@users.noreply.github.com>
2023-09-04 14:02:54 +0200

    Fix SQL API endpoint

--- api/sql.py
+++ api/sql.py
@@ -1,10 +1,10 @@
-from api.chartgpt import get_valid_sql_query
+from api.chartgpt import SQLExecutionResult, get_valid_sql_query
 
 
 def generate_sql(body):
     question = body['question']
     
     # Logic to generate SQL query based on question
-    _description, query = get_valid_sql_query(question)
+    query_result: SQLExecutionResult = get_valid_sql_query(question)
 
-    return {"query": query}, 200
+    return {"query": query_result.query}, 200
