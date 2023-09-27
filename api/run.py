eda08fd43da15ae7018b902133a29b5e95f50a9f
BenSchZA <BenSchZA@users.noreply.github.com>
2023-09-27 23:29:39 +0200

    Update keep-alive event

--- api/run.py
+++ api/run.py
@@ -98,7 +98,8 @@ async def keep_alive_generator(queue: asyncio.Queue, stop_event: asyncio.Event) 
     try:
         while not stop_event.is_set():
             await asyncio.sleep(15)
-            await queue.put(":keep-alive\n\n")
+            await queue.put(r"event: keep-alive\n")
+            await queue.put(r"data: {}\n\n")
     except asyncio.CancelledError:
         pass
 
